02b4ca1529d22f376f7041ef9a7fef73bec86721
Vladi Ciuculescu <vladimir.ciuculescu@titeica.com>
2021-05-26 14:06:55 +0300

    Pe asta lucram

--- src/get_php_link.js
+++ src/get_php_link.js
@@ -1,2 +1,2 @@
-var pathString = window.location.href.includes('http://localhost:3') ? 'http://localhost/nokia/php' : './php';
+var pathString = window.location.href.includes('http://localhost:3') ? 'http://localhost/NOKIA-entire-project/php' : './php';
 export default pathString;
